d280198fd520f974390ea93c755c5db65bb51043
anemonadja <nadja.villarroel.n@gmail.com>
2025-08-02 05:42:45 -0400

    favicon

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,21 @@ function App() {
   return (
     <>
       <div>
+        <main>
+          <h1>Vite + React</h1>
+        </main>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +31,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
